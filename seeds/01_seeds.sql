75ae4fdc6023bfb8e2e0d909058d47efc7c022f5
maryanibrahim <maryanibrahim301@yahoo.com>
2023-08-05 23:57:21 +0000

    add data to all tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO users (name, email, password)
+VALUES
+  ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+  ('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+  ('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES
+  (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', 'true'),
+  (2, 'Blank corner', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 85234, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', '83680', 'true'),
+  (3, 'Habit mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 46058, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', 'true');
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES
+  ('2023-08-01', '2023-08-10', 1, 2),
+  ('2023-08-11', '2023-08-20', 2, 3),
+  ('2023-08-21', '2023-08-30', 3, 1);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) 
+VALUES
+  (2, 1, 1, 3, 'message'),  
+  (3, 2, 2, 4, 'message'),  
+  (1, 3, 3, 5, 'message');
